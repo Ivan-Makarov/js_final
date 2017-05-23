a3628f39eb5bdde2ad7cb4717e99c9796e578b52
Ivan Makarov <ivan.s.makarov@yandex.ru>
2017-05-24 00:34:23 +0300

    created class Vector

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class Vector {
+    constructor(coordX = 0, coordY = 0) {
+        this.x = coordX;
+        this.y = coordY;
+    }
+
+    plus(vector) {
+        if (!Vector.prototype.isPrototypeOf(vector)) {
+            throw new Error('Можно прибавлять к вектору только вектор типа Vector');
+        }
+        let newX = this.x + vector.x;
+        let newY = this.y + vector.y;
+        return new Vector(newX, newY)
+    }
+
+    times(multiplier) {
+        let newX = this.x * multiplier;
+        let newY = this.y * multiplier;
+        return new Vector(newX, newY)
+    }
+}
